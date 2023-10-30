8457f95dc1d1adc454d5121a77fa1a7ddfe07c78
Saya <fl.daniela@javeriana.edu.co>
2023-10-30 14:48:23 -0500

    Intereses

---
+++ app/src/main/java/com/saycs/saycs/InteresesActivity.kt
@@ -0,0 +1,11 @@
+package com.saycs.saycs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InteresesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intereses)
+    }
+}
