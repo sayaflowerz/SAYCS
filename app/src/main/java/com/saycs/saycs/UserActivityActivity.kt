b37095dbbc60828bc8ed55f2ee045b4f055bb686
Saya <fl.daniela@javeriana.edu.co>
2023-11-19 23:34:54 -0500

    Pantallas

---
+++ app/src/main/java/com/saycs/saycs/UserActivityActivity.kt
@@ -0,0 +1,11 @@
+package com.saycs.saycs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_activity)
+    }
+}
